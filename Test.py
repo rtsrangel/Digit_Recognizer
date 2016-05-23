611a3ee42ef96fb4c5d23c0d26d633dc9ec33482
rtsrangel <johnrangel842@gmail.com>
2016-05-23 14:06:12 -0700

    Import Test

--- Test.py
+++ Test.py
@@ -1,1 +1,16 @@
+import cv2
+import numpy as np
+import socket
+import time
+import multiprocessing
+import mutex
 print "hello world"
+
+cap = cv2.VideoCapture(0)
+
+while (True):
+    frame = cap.read()
+    cv2.imshow('Image Name', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
