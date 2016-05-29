62317725d812e9ea2ba7ae07e7c7c0dd7260c148
rtsrangel <johnrangel842@gmail.com>
2016-05-28 23:51:03 -0700

    SVC Finished
    Cleaned up code and comments.
    Current accuracy is about ~96.8%
    Will come back to when I get farther in Udacity nanodegree.

--- SVC_Approach.py
+++ SVC_Approach.py
@@ -1,9 +1,5 @@
 import pandas as pd
-import numpy as np
-from sklearn import linear_model
 from sklearn import cross_validation
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import PolynomialFeatures
 from sklearn import svm
 import time
 
@@ -17,21 +13,22 @@ test_url = 'C:/Users/krazy_000/Downloads/Kaggle Digit Project/test.csv'
 train = pd.read_csv(train_url)
 test = pd.read_csv(test_url)
 
-# Seperate train data into data and target
+# Separate train data into data and target
 data = train.loc[:, 'pixel0':]
 target = train.loc[:, 'label']
 
 # Split training data using cross validation
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(data, target, test_size=0.4, random_state=1)
 
-# Apply simple linear regression on training data
-clf = svm.SVC(kernel='poly', C=.01)
+# Apply Support Vector Classifier to training data
+clf = svm.SVC(kernel='poly', C=.01, degree=3)
 clf.fit(X_train, y_train)
 print clf.score(X_test, y_test)
 
 # Make predictions on test data and put into a dataframe
 df = pd.DataFrame({"ImageId": range(1, test.shape[0]+1), 'Label': clf.predict(test)})
 df = df.round(0)
-df.to_csv("submission 2.csv", float_format='%.f', index=False)
+df.to_csv("submission 3.csv", float_format='%.f', index=False)
 
+# Print run time
 print("--- %s seconds ---" % (time.time() - start_time))
