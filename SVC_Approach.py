c445a65961b21444662f42588c91376e58d9e169
rtsrangel <johnrangel842@gmail.com>
2016-05-28 17:31:35 -0700

    Revert "Added Comments"
    This reverts commit a500fea3f40d125f5dbd03f20a865afc71a10aa1.

--- SVC_Approach.py
+++ SVC_Approach.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import numpy as np
+from sklearn import linear_model
 from sklearn import cross_validation
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn import svm
 import time
 
-# Initialize timer
 start_time = time.time()
 
 # Change url to local location of data
@@ -14,23 +17,21 @@ test_url = 'C:/Users/krazy_000/Downloads/Kaggle Digit Project/test.csv'
 train = pd.read_csv(train_url)
 test = pd.read_csv(test_url)
 
-# Separate train data into data and target
+# Seperate train data into data and target
 data = train.loc[:, 'pixel0':]
 target = train.loc[:, 'label']
 
 # Split training data using cross validation
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(data, target, test_size=0.4, random_state=1)
 
-# Apply SVC on the training data
+# Apply simple linear regression on training data
 clf = svm.SVC(kernel='poly', C=.01)
 clf.fit(X_train, y_train)
-# Print accuracy of the testing data
 print clf.score(X_test, y_test)
 
-# Make predictions on competition data and put into a dataframe
+# Make predictions on test data and put into a dataframe
 df = pd.DataFrame({"ImageId": range(1, test.shape[0]+1), 'Label': clf.predict(test)})
 df = df.round(0)
 df.to_csv("submission 2.csv", float_format='%.f', index=False)
 
-# Print run time
 print("--- %s seconds ---" % (time.time() - start_time))
